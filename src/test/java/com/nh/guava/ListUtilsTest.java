ff927a984141a71a81e01bec2b907f9130d52b27
liliang <649743135@qq.com>
2016-08-07 12:00:24 +0800

    junit3 ---> junit4

--- src/test/java/com/nh/guava/ListUtilsTest.java
+++ src/test/java/com/nh/guava/ListUtilsTest.java
@@ -1,19 +1,20 @@
 package com.nh.guava;
 
 import com.google.common.base.Splitter;
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.util.List;
 
 /**
  * Created by liliang on 2016/7/21.
  */
-public class ListUtilsTest extends TestCase{
+public class ListUtilsTest {
 
+    @Test
     public void testSplitList()
     {
         String ids = "1,2,3,4,5";
         List<String> idList = Splitter.on(",").trimResults().splitToList(ids);
-        idList.size();
+        idList.forEach((x)-> System.out.println(x));
     }
 }
